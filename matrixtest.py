cf4b6ad78df4a5cf7851e01dbd9b918fee5d159d
Phillip Burgess <paintyourdragon@dslextreme.com>
2015-01-14 17:42:12 -0800

    Oops, had a section of demo commented out

--- matrixtest.py
+++ matrixtest.py
@@ -7,12 +7,12 @@ from rgbmatrix import Adafruit_RGBmatrix
 matrix = Adafruit_RGBmatrix(32, 1)
 
 # Flash screen red, green, blue (packed color values)
-#matrix.Fill(0xFF0000)
-#time.sleep(1.0)
-#matrix.Fill(0x00FF00)
-#time.sleep(1.0)
-#matrix.Fill(0x0000FF)
-#time.sleep(1.0)
+matrix.Fill(0xFF0000)
+time.sleep(1.0)
+matrix.Fill(0x00FF00)
+time.sleep(1.0)
+matrix.Fill(0x0000FF)
+time.sleep(1.0)
 
 # Show RGB test pattern (separate R, G, B color values)
 for b in range(16):
