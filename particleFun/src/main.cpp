d6f1f7a5784197a51c78063782b10b74c01a2f6f
zmrubin <pi@raspberrypi.(none)>
2015-02-12 19:50:07 +0000

    builds on raspi

--- particleFun/src/main.cpp
+++ particleFun/src/main.cpp
@@ -1,10 +1,17 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
+#define RPI
 //--------------------------------------------------------------
 int main(){
-	ofAppGlutWindow window; // create a window
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 64*1, 32*1, OF_WINDOW);
-	ofRunApp(new testApp()); // start the app
-}
+  #ifndef RPI
+    ofAppGlutWindow window; // create a window
+       // RPI
+        // set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+            ofSetupOpenGL(&window, 64*1, 32*1, OF_WINDOW);
+              #else
+                ofSetupOpenGL(64*1, 32*1, OF_WINDOW);
+                  #endif
+                    ofRunApp(new testApp()); // start the app
+                    }
+
