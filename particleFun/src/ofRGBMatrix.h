1e408c7ee98626f524ad23fd8feeaa2e099e5710
zmrubin <pi@raspberrypi.(none)>
2015-03-08 00:27:26 +0000

    Aah! line endings

--- particleFun/src/ofRGBMatrix.h
+++ particleFun/src/ofRGBMatrix.h
@@ -1,1 +1,49 @@
-#pragma once#include "ofMain.h"#define ADAFRUIT_RGBMATRIX_HAT#include "led-matrix.h"#include <unistd.h>using rgb_matrix::GPIO;using rgb_matrix::RGBMatrix;using rgb_matrix::Canvas;//#include "ofxTextSuite.h"class ofRGBMatrix:public ofThread{    Canvas *canvas;    void threadedFunction ()    {        GPIO io;        if (!io.Init ())        {            printf ("IO ERROR: are you root?");            return;        }        canvas = new RGBMatrix (&io, 32, 2);        while (isThreadRunning())        {            continue;        }        canvas->Clear();        delete canvas;    }public:    void setPixel (int x, int y, int r, int g, int b)    {        canvas->SetPixel (x, y, r, g, b);    }    void fill(int r, int g, int b)    {        canvas->Fill(r,g,b);    }    void clear()    {        canvas->Clear();    }};
+#pragma once
+
+#include "ofMain.h"
+
+#define ADAFRUIT_RGBMATRIX_HAT
+#include "led-matrix.h"
+#include <unistd.h>
+
+using rgb_matrix::GPIO;
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
+
+
+//#include "ofxTextSuite.h"
+
+class ofRGBMatrix:public ofThread
+{
+    Canvas *canvas;
+    void threadedFunction ()
+    {
+        GPIO io;
+        if (!io.Init ())
+        {
+            printf ("IO ERROR: are you root?");
+            return;
+        }
+        canvas = new RGBMatrix (&io, 32, 2);
+
+        while (isThreadRunning())
+        {
+            continue;
+        }
+        canvas->Clear();
+        delete canvas;
+    }
+public:
+    void setPixel (int x, int y, int r, int g, int b)
+    {
+        canvas->SetPixel (x, y, r, g, b);
+    }
+    void fill(int r, int g, int b)
+    {
+        canvas->Fill(r,g,b);
+    }
+    void clear()
+    {
+        canvas->Clear();
+    }
+};
--- particleFun/src/universe.cpp
+++ particleFun/src/universe.cpp
@@ -1,1 +1,107 @@
-#include "universe.h"Universe::Universe(){    offScreenCharges.allocate(ofGetWidth(), ofGetHeight(),GL_RGB);    offScreenCharges.begin();    ofClear(0,0,0,0);    offScreenCharges.end();    verdana1.loadFont("verdana.ttf", ofGetWidth()*.155, true, true, true);//loadFont("verdana1.ttf", 70, true, true, true);    verdana1.setLineHeight(34.0f);    verdana1.setLetterSpacing(1.035);}void Universe::refString(string str){    offScreenCharges.begin();    ofClear(0,0,0,0);    //ofPushMatrix();    ofSetColor(255,255,255);    ofRectangle bounds = verdana1.getStringBoundingBox(str, 0,0);    verdana1.drawStringAsShapes(str, (ofGetWidth()-bounds.width)/2, ofGetHeight()/2+bounds.height/2);    // ofPopMatrix();    offScreenCharges.end();    offScreenCharges.readToPixels(oscPix);    float x = 0;    float y = 0;    vector<Particle*>::iterator it = particles.begin();    for (float h = 0; h<ofGetHeight(); h++)    {        for (float w = 0; w<ofGetWidth()*3; w+=3)        {            if(oscPix[w+h*ofGetWidth()*3] == 255 )            {                (*it)->target(ofVec2f(x,y));//ofGetWidth()/2 , ofGetHeight()/2));                //(*it)->pos = ofVec2f(x,y);                (*it)->forceType = DIRECT;                //  printf("%i \n", oscPix[w+h*ofGetWidth()*3]);                it++;            }            else if (oscPix[w+h*ofGetWidth()*3] == 128)            {                continue; //reserved            }            x++;        }        x = 0;        y ++;    }    while (it != particles.end())    {        (*it)->forceType = CHARGE;        it++;    }}void Universe::update(){    normFactor = 60.0/ofGetFrameRate();    vector<Particle*>::iterator it;    for(it=particles.begin(); it != particles.end(); it++)    {        static float lastTime = 1;        float dt = lastTime - ofGetElapsedTimef();        lastTime = (float)ofGetElapsedTimef();        if ((*it)->forceType == DIRECT)        {            float kp = 100;            float ki =1;            float kd = -10000;            ofVec2f error_diff = (*it)->anchor_e - ((*it)->pos - (*it)->anchor);            (*it)->anchor_e = (*it)->pos - (*it)->anchor;            (*it)->anchor_eacc += (*it)->anchor_e;            (*it)->force = - ( (*it)->anchor_e*kp + (*it)->anchor_eacc*ki + error_diff*kd ) ;        }        vector<Particle*>::iterator jt;        for(jt=it; jt != particles.end(); jt++)        {            if(((*it)->forceType == CHARGE) || ((*jt)->forceType == CHARGE))            {                ofVec2f unit = ((*jt)->pos - (*it)->pos).normalize();                ofVec2f f = unit * (*it)->charge * (*jt)->charge/((*it)->pos.distanceSquared((*jt)->pos));                (*it)->force -= f;                (*jt)->force +=f;            }        }        (*it)->update();    }}Universe::~Universe(){    //dtor}
+#include "universe.h"
+
+Universe::Universe()
+{
+    offScreenCharges.allocate(ofGetWidth(), ofGetHeight(),GL_RGB);
+    offScreenCharges.begin();
+    ofClear(0,0,0,0);
+    offScreenCharges.end();
+    verdana1.loadFont("verdana.ttf", ofGetWidth()*.155, true, true, true);//loadFont("verdana1.ttf", 70, true, true, true);
+    verdana1.setLineHeight(34.0f);
+    verdana1.setLetterSpacing(1.035);
+}
+
+void Universe::refString(string str)
+{
+    offScreenCharges.begin();
+    ofClear(0,0,0,0);
+    //ofPushMatrix();
+    ofSetColor(255,255,255);
+    ofRectangle bounds = verdana1.getStringBoundingBox(str, 0,0);
+    verdana1.drawStringAsShapes(str, (ofGetWidth()-bounds.width)/2, ofGetHeight()/2+bounds.height/2);
+    // ofPopMatrix();
+    offScreenCharges.end();
+    offScreenCharges.readToPixels(oscPix);
+    float x = 0;
+    float y = 0;
+    vector<Particle*>::iterator it = particles.begin();
+    for (float h = 0; h<ofGetHeight(); h++)
+    {
+        for (float w = 0; w<ofGetWidth()*3; w+=3)
+        {
+            if(oscPix[w+h*ofGetWidth()*3] == 255 )
+            {
+                (*it)->target(ofVec2f(x,y));//ofGetWidth()/2 , ofGetHeight()/2));
+                //(*it)->pos = ofVec2f(x,y);
+                (*it)->forceType = DIRECT;
+                //  printf("%i \n", oscPix[w+h*ofGetWidth()*3]);
+                it++;
+
+            }
+            else if (oscPix[w+h*ofGetWidth()*3] == 128)
+            {
+                continue; //reserved
+            }
+            x++;
+        }
+        x = 0;
+        y ++;
+    }
+    while (it != particles.end())
+    {
+        (*it)->forceType = CHARGE;
+        it++;
+    }
+}
+
+
+void Universe::update()
+{
+    normFactor = 60.0/ofGetFrameRate();
+    vector<Particle*>::iterator it;
+
+    for(it=particles.begin(); it != particles.end(); it++)
+    {
+        static float lastTime = 1;
+        float dt = lastTime - ofGetElapsedTimef();
+        lastTime = (float)ofGetElapsedTimef();
+
+
+        if ((*it)->forceType == DIRECT)
+        {
+            float kp = 100;
+            float ki =1;
+            float kd = -10000;
+            ofVec2f error_diff = (*it)->anchor_e - ((*it)->pos - (*it)->anchor);
+            (*it)->anchor_e = (*it)->pos - (*it)->anchor;
+            (*it)->anchor_eacc += (*it)->anchor_e;
+
+            (*it)->force = - ( (*it)->anchor_e*kp + (*it)->anchor_eacc*ki + error_diff*kd ) ;
+        }
+
+
+        vector<Particle*>::iterator jt;
+
+        for(jt=it; jt != particles.end(); jt++)
+        {
+
+            if(((*it)->forceType == CHARGE) || ((*jt)->forceType == CHARGE))
+            {
+                ofVec2f unit = ((*jt)->pos - (*it)->pos).normalize();
+                ofVec2f f = unit * (*it)->charge * (*jt)->charge/((*it)->pos.distanceSquared((*jt)->pos));
+                (*it)->force -= f;
+                (*jt)->force +=f;
+            }
+        }
+
+
+        (*it)->update();
+    }
+
+
+}
+
+Universe::~Universe()
+{
+    //dtor
+}
